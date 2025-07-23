bb6c84378ce9a7730be3fb3b95bd0b8e4cc14931
Hash <denisyrt02@gmail.com>
2025-07-23 17:27:36 +0300

    `AssemblyInfo` update.

--- TaskBasedBackgroundWorkers/Properties/AssemblyInfo.cs
+++ TaskBasedBackgroundWorkers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleAppNetFramewokr1")]
+[assembly: AssemblyTitle("TaskBasedBackgroundWorkers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleAppNetFramewokr1")]
+[assembly: AssemblyProduct("TaskBasedBackgroundWorkers")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
