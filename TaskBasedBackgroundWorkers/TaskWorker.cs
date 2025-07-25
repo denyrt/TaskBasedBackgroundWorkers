03222b482af588bb5911f61331ff9838595ec8c9
Hash <39876327+denyrt@users.noreply.github.com>
2025-07-26 01:17:39 +0300

    Feature/concurrent sync (#4)
    * Redundant cancel call removed. 
    
    * Intoduced cancellation callback.
    
    * Clean-up moved to cancellation callback.
    
    * Added missing comments and changed existed comments.
    
    * Clean-up when task failed with unhandled exception.
    
    * `OnStarted`, `OnStopped`, `OnExceptionThrown` are private so not virtual too.
    
    * `IProgress` instance is now provided within `DoWorkAsync` and `OnProgressChanged` is private as well.
    
    * Examples reworked.
    
    * Added `Debug` for `TaskWorker` to track its life-time.

--- TaskBasedBackgroundWorkers/TaskWorker.cs
+++ TaskBasedBackgroundWorkers/TaskWorker.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,29 +10,41 @@ namespace TaskBasedBackgroundWorkers
 {
     /// <summary>
     /// A base type for a thread-safe re-runnable worker that supports stop using its own stop method or via external <see cref="CancellationToken"/>.
+    /// It is wrapped around underlying task which behaviour could be adjusted by passing <see cref="TaskFactory"/>, <see cref="TaskScheduler"/> 
+    /// and <see cref="TaskCreationOptions"/>. So worker behaviour could be setuped for both long-time execution and short-time execution.
     /// </summary>
     /// <remarks>
     /// A few notes:
     /// <list type="bullet">
-    /// Worker could be re-start again if <see cref="IsRunning"/> is <see langword="false"/>.
+    /// Worker could be re-started again if <see cref="IsRunning"/> is <see langword="false"/>.
     /// </list>
     /// <list type="bullet"> 
-    /// All start and stop methods are concurrent synchronized in way that only a of these method could be executed at one moment of time.
-    /// </list>
-    /// <list type="bullet">
-    /// Worker stopping using external <see cref="CancellationToken"/> shares the same synchronized behaivour with start and stop methods.
-    /// So external call cancellationTokenSource.Cancel() will not be performed in one moment of time with start and stop methods.
+    /// Underlying task setup and clean-up are concurrent synchronized in way that only a of them could be executed at one moment of time.
+    /// Worker stopping using either external <see cref="CancellationToken"/> or method <see cref="Stop"/> uses sync for concurrent calls,
+    /// so underlying resources associated with running task could be accessed only from one thread. The same sync is used for start methods.
+    /// It is required to prevent invalid behaviour when both start and stop called from diffent threads in one moment of time.
     /// </list>
     /// </remarks>
     /// <typeparam name="TProgress"> Type that represents progress of worker do-work execution. </typeparam>
     public abstract class TaskWorker<TProgress> : IDisposable
     {
-        private readonly TaskFactory            _taskFactory;
-        private readonly TaskCreationOptions    _taskCreationOptions;
-        private readonly SemaphoreSlim          _semaphoreSlim;
+        // The underlying task factory. TaskScheduler must be present within.
+        private readonly TaskFactory _taskFactory;
 
-        private CancellationTokenSource         _cts;
-        private Task                            _task;
+        // The underlying creation options.
+        private readonly TaskCreationOptions _taskCreationOptions;
+
+        // The synchronization of underlying task setup/clean-up to prevent concurrent changes of its state.
+        private readonly SemaphoreSlim _semaphoreSlim;
+
+        // The underlying cancellation source.
+        private CancellationTokenSource _cts;
+
+        // The handler that performs clean-up in if task was cancelled.
+        private CancellationTokenRegistration? _cleanUpCtr;
+
+        // The underlying task.
+        private Task _task;
 
         /// <summary>
         /// Indicates if worker is still running.
@@ -45,6 +59,9 @@ namespace TaskBasedBackgroundWorkers
         /// <summary>
         /// Raises when worker is stopped.
         /// </summary>
+        /// <remarks>
+        /// The underlying task and its resources are cleaned-up already when this event is raised.
+        /// </remarks>
         public event EventHandler<TaskWorkerStoppedEventArgs> Stopped;
 
         /// <summary>
@@ -55,6 +72,9 @@ namespace TaskBasedBackgroundWorkers
         /// <summary>
         /// Raises when do-work method failed with unxpected exception.
         /// </summary>
+        /// <remarks>
+        /// The underlying task and its resources are cleaned-up already when this event is raised.
+        /// </remarks>
         public event EventHandler<TaskWorkerExceptionEventArgs> ExceptionThrown;
 
         /// <summary>
@@ -84,6 +104,8 @@ namespace TaskBasedBackgroundWorkers
         /// </remarks>
         public TaskWorker(TaskFactory taskFactory, TaskCreationOptions taskCreationOptions)
         {
+            DebugEnter();
+
             if (taskFactory.Scheduler == null)
             {
                 throw new ArgumentException($"Cannot accept task factory without scheduler.", nameof(taskFactory.Scheduler));
@@ -92,18 +114,23 @@ namespace TaskBasedBackgroundWorkers
             _taskFactory = taskFactory;
             _taskCreationOptions = taskCreationOptions;
             _semaphoreSlim = new SemaphoreSlim(1, 1);
+
+            DebugExit();
         }
 
         /// <summary>
         /// Work that is executed by worker on start.
         /// </summary>
+        /// <param name="progress">
+        /// A progress that could be used to raise <see cref="ProgressChanged"/>. Its life-time is bound to task-associated resources.
+        /// </param>
         /// <param name="cancellationToken"> 
         /// Cancellation token provided by worker.
         /// </param>
         /// <returns> 
         /// A task that represents async operation. 
         /// </returns>
-        protected abstract Task DoWorkAsync(CancellationToken cancellationToken);
+        protected abstract Task DoWorkAsync(IProgress<TProgress> progress, CancellationToken cancellationToken);
 
         /// <summary> 
         /// Raises <see cref="Started"/> event.
@@ -112,9 +139,13 @@ namespace TaskBasedBackgroundWorkers
         /// <remarks>
         /// Called right before <see cref="DoWorkAsync(CancellationToken)"/>. Inner task is already running at moment of this call and <see cref="IsRunning"/> is <see langword="true"/>.
         /// </remarks>
-        protected virtual void OnStarted(TaskWorkerStartedEventArgs e)
+        private void OnStarted(TaskWorkerStartedEventArgs e)
         {
+            DebugEnter();
+
             Started?.Invoke(this, e);
+
+            DebugExit();
         }
 
         /// <summary> 
@@ -124,18 +155,26 @@ namespace TaskBasedBackgroundWorkers
         /// <remarks>
         /// Called when <see cref="DoWorkAsync(CancellationToken)"/> finished execution.
         /// </remarks>
-        protected virtual void OnStopped(TaskWorkerStoppedEventArgs e)
+        private void OnStopped(TaskWorkerStoppedEventArgs e)
         {
+            DebugEnter();
+
             Stopped?.Invoke(this, e);
+
+            DebugExit();
         }
 
         /// <summary>
         /// Raises <see cref="ProgressChanged"/> event.
         /// </summary>
         /// <param name="e"> Value of progress. </param>
-        protected virtual void OnProgressChanged(TaskWorkerProgressChangedEventArgs<TProgress> e)
+        private void OnProgressChanged(TaskWorkerProgressChangedEventArgs<TProgress> e)
         {
+            DebugEnter();
+
             ProgressChanged?.Invoke(this, e);
+
+            DebugExit();
         }
 
         /// <summary>
@@ -145,11 +184,22 @@ namespace TaskBasedBackgroundWorkers
         /// <remarks>
         /// Called when <see cref="DoWorkAsync(CancellationToken)"/> fault with unhandled exception.
         /// </remarks>
-        protected virtual void OnExceptionThrown(TaskWorkerExceptionEventArgs e)
+        private void OnExceptionThrown(TaskWorkerExceptionEventArgs e)
         {
+            DebugEnter();
+
             ExceptionThrown?.Invoke(this, e);
+
+            DebugExit();
         }
 
+        /// <summary>
+        /// Starts worker that executes do-work once upon its run.
+        /// </summary>
+        /// <param name="linkedToken"> 
+        /// A bound cancellation token. Cancellation request this token will lead to stop of current worker. 
+        /// </param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void Start(CancellationToken linkedToken = default)
         {
             var tokens = new CancellationToken[1] { linkedToken };
@@ -158,12 +208,19 @@ namespace TaskBasedBackgroundWorkers
             {
                 Start(tokens);
             }
-            catch (InvalidOperationException ex) 
+            catch (InvalidOperationException ex)
             {
                 throw ex;
             }
         }
 
+        /// <summary>
+        /// Starts worker that executes do-work once upon its run.
+        /// </summary>
+        /// <param name="linkedTokens"> 
+        /// A bunch of bound cancellation tokens. Cancellation request from one of these tokens will lead to stop of current worker. 
+        /// </param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void Start(IEnumerable<CancellationToken> linkedTokens)
         {
             var tokens = linkedTokens.ToArray();
@@ -179,7 +236,7 @@ namespace TaskBasedBackgroundWorkers
         }
 
         /// <summary>
-        /// Starts worker that executes <see cref="DoWorkAsync(CancellationToken)"/> once upon its run.
+        /// Starts worker that executes do-work once upon its run.
         /// </summary>
         /// <param name="linkedTokens"> 
         /// A bunch of bound cancellation tokens. Cancellation request from one of these tokens will lead to stop of current worker. 
@@ -196,7 +253,7 @@ namespace TaskBasedBackgroundWorkers
             {
                 throw new InvalidOperationException($"Array '{linkedTokens}' cannot be empty.");
             }
-            
+
             _semaphoreSlim.Wait();
 
             try
@@ -262,74 +319,37 @@ namespace TaskBasedBackgroundWorkers
                 throw new InvalidOperationException("Cannot stop not running task.");
             }
 
-            _semaphoreSlim.Wait();
-
-            try
-            {
-                _cts.Cancel();
-            }
-            finally
-            {
-                _semaphoreSlim.Release();
-            }
-        }
-
-        /// <summary>
-        /// Sends cancellation signal that lead to stopping of worker and releasing of resources associeted with running task.
-        /// </summary>
-        /// <param name="ct"> Cancellation token. </param>
-        /// <returns></returns>
-        /// <exception cref="InvalidOperationException"></exception>
-        /// <remarks>
-        /// Current method not waits for task-worker execution. Awaiting here is applied only to synchronization of concurrent calls. 
-        /// </remarks>
-        public async Task StopAsync(CancellationToken ct = default)
-        {
-            if (!IsRunning)
-            {
-                // ToDo: Consider an alernative exception-safe approach with 'IsRunning' check?
-                throw new InvalidOperationException("Cannot stop not running task.");
-            }
-
-            await _semaphoreSlim.WaitAsync(ct);
-
             _cts.Cancel();
-
-            try
-            {
-                _cts.Cancel();
-            }
-            finally
-            {
-                _semaphoreSlim.Release();
-            }
         }
 
+        // Setup of task and its associated resources.
         private void CreateAndStartTask(CancellationTokenSource cts)
         {
             _cts = cts;
 
             var ct = _cts.Token;
+
+            _cleanUpCtr = ct.Register(CleanupTaskResources);
+
             var func = new Action<CancellationToken>(async (token) => await ExecuteDoWorkAsync(token));
 
             _task = _taskFactory.StartNew(() => func(ct), ct, _taskCreationOptions, _taskFactory.Scheduler);
         }
 
+        // A method that is passed to underlying task on its creation.
         private async Task ExecuteDoWorkAsync(CancellationToken cancellationToken)
         {
             OnStarted(TaskWorkerStartedEventArgs.Empty);
 
             try
             {
-                try
+                using (var progress = TaskWorkerProgress<TProgress>.FromHandler(OnProgressChanged))
                 {
-                    await DoWorkAsync(cancellationToken).ConfigureAwait(false);
+                    await DoWorkAsync(progress, cancellationToken).ConfigureAwait(false);
                 }
-                finally
-                {
-                    CleanupTaskResources();
-                }
-                
+
+                CleanupTaskResources();
+
                 OnStopped(TaskWorkerStoppedEventArgs.Finished);
             }
             catch (TaskCanceledException)
@@ -338,11 +358,25 @@ namespace TaskBasedBackgroundWorkers
             }
             catch (Exception ex)
             {
+                CleanupTaskResources();
+
                 OnExceptionThrown(new TaskWorkerExceptionEventArgs(ex));
+
                 OnStopped(TaskWorkerStoppedEventArgs.Exception);
             }
         }
 
+        // Clean-up underlying cancellation registration (callbck.
+        private void CleanupCTR()
+        {
+            if (_cleanUpCtr.HasValue)
+            {
+                _cleanUpCtr.Value.Dispose();
+                _cleanUpCtr = null;
+            }
+        }
+
+        // Clean-up underlying cancellation source.
         private void CleanupCTS()
         {
             if (_cts != null)
@@ -352,6 +386,7 @@ namespace TaskBasedBackgroundWorkers
             }
         }
 
+        // Clean-up underlying task.
         private void CleanupTask()
         {
             if (_task != null)
@@ -365,39 +400,93 @@ namespace TaskBasedBackgroundWorkers
             }
         }
 
+        // Organized clean-up of task associated resources.
         private void CleanupTaskResources()
         {
-            CleanupCTS();
-            CleanupTask();
+            DebugEnter();
+
+            try
+            {
+                _semaphoreSlim.Wait();
+
+                CleanupCTR();
+                CleanupCTS();
+                CleanupTask();
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
+
+            DebugExit();
         }
 
+        // Organized clean-up of exposed event handlers.
         private void CleanupEvents()
         {
+            DebugEnter();
+
             Started = null;
             Stopped = null;
             ProgressChanged = null;
             ExceptionThrown = null;
+
+            DebugExit();
         }
 
+        // Organized clean-up of other underlying objects.
         private void CleanupInstance()
         {
+            DebugEnter();
+
             _semaphoreSlim.Dispose();
+
+            DebugExit();
         }
 
         protected virtual void Dispose(bool disposing)
         {
+            DebugEnter();
+
             if (disposing)
             {
                 CleanupTaskResources();
                 CleanupEvents();
                 CleanupInstance();
             }
+
+            DebugExit();
         }
 
+        /// <summary>
+        /// Releases all resources used by current instance of <see cref="TaskWorker{TProgress}"/>.
+        /// </summary>
         public void Dispose()
         {
+            DebugEnter();
+
             Dispose(true);
             GC.SuppressFinalize(this);
+
+            DebugExit();
+        }
+
+        [Conditional("DEBUG")]
+        private static void Debug(string message, [CallerMemberName] string callerName = null)
+        {
+            System.Diagnostics.Debug.WriteLine($"{callerName} | {message}", $"{typeof(TaskWorker<>)}");
+        }
+
+        [Conditional("DEBUG")]
+        private static void DebugEnter([CallerMemberName] string callerName = null)
+        {
+            Debug("Enter.", callerName);
+        }
+
+        [Conditional("DEBUG")]
+        private static void DebugExit([CallerMemberName] string callerName = null)
+        {
+            Debug("Exit.", callerName);
         }
     }
 }
